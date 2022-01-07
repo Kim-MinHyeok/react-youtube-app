839182bec07352a3fd15b41e0ea14eeb877f4b72
Kim-MinHyeok <48408314+Kim-MinHyeok@users.noreply.github.com>
2022-01-07 22:39:46 +0900

    first commit

---
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios';
+
+function Comment() {
+    return (
+        <div>
+            Comment
+        </div>
+    )
+}
+
+export default Comment
